35f9f96f471a573fc8295a7f1a15169fcb406bc0
Simanto4038 <86630073+Simanto4038@users.noreply.github.com>
2021-11-19 12:42:33 +0600

    My-Order

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ require('dotenv').config();
 const app = express()
 const ObjectId = require('mongodb').ObjectId;
 const email = require('mongodb').email;
-const port = process.env.PORT || 5000;
+// const http = require('http');
+// const server = http.createServer(app);
+//const io = new Server(httpServer, { /* options */ });
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "*"
+    
+//   }
+// });
+
 const cors = require('cors');
 var bodyParser = require('body-parser');
+// parse application/json
+app.use(bodyParser.json())
 app.use(cors())
+
+const port = process.env.PORT || 5000;
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bbopj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -17,6 +31,7 @@ app.get('/', (req, res) => {
     res.send('WE ARE BANGLADESH 4 WHEELERS HUB')
   })
 
+ 
 
   async function run() {
     try {
@@ -24,8 +39,24 @@ app.get('/', (req, res) => {
       const database = client.db("Tesla4wheeler");
       const allVhclCollection = database.collection("CatagoryCollection");
       const slider1Collection = database.collection("slider");
-      const slider2Collection = database.collection("Slider2");
-    
+      const commentDataCollection = database.collection("Comments");
+      const  orderCollection = database.collection("USERoRDER");
+     
+
+      
+         
+
+        //Post A Document to Data base
+
+      
+     app.post("/allcatagogy",async(req,res)=>
+      {
+        const newData = req.body;
+        console.log('Send to DB',req.body);
+        const result = await  allVhclCollection.insertOne(newData);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        res.json(result)
+      })
 
 
     //   app.get('/catagorydetail', async (req, res) => {
@@ -72,6 +103,27 @@ app.get('/', (req, res) => {
       const users = await result.toArray()
       res.send(users)
       })
+    
+         //POST a Comment
+
+
+
+         app.post("/comment",async(req,res)=>
+         {
+           const newcomment = req.body;
+           console.log('Send to DB',req.body);
+           const result = await  commentDataCollection.insertOne(newcomment);
+           console.log(`A comment was inserted with the _id: ${result.insertedId}`);
+           res.json(result)
+         })
+        
+            app.get('/comment', async (req, res) => {
+
+        const result = commentDataCollection.find({});
+        const users = await result.toArray()
+        res.send(users)
+           })
+
 
       //claints Data Get
 
@@ -100,47 +152,30 @@ app.get('/', (req, res) => {
           
        /*********Post A  Dynamic Cart Document to Data base**********                      */
 
-    //   app.post("/userOrder",async(req,res)=>
-    //   {
-    //     const order = req.body;
-    //     console.log('Send Cart to DB' , order);
-    //     const result = await  orderCollection.insertOne(order);
-    //     console.log(`A Cart was inserted with the _id: ${result.insertedId}`);
-    //     res.json(result)
+      app.post("/userOrder",async(req,res)=>
+      {
+        const order = req.body;
+        console.log('Send Cart to DB' , order);
+        const result = await  orderCollection.insertOne(order);
+        console.log(`A Cart was inserted with the _id: ${result.insertedId}`);
+        res.json(result)
       
-    //   })
+      })
 
 
 
+        
+      app.get('/userOrder', async (req, res) => {
+
+        const result = orderCollection.find({});
+        const users = await result.toArray()
+        res.send(users)
+        })
+
+    
 
 
-      //Post A Document to Data base
-
-      
-    //  app.post("/userInput",async(req,res)=>
-    //   {
-    //     const newUser = req.body;
-    //     console.log('Send to DB',req.body);
-    //     const result = await  userDataCollection.insertOne(newUser);
-    //     console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    //     res.json(result)
-    //   })
-
-
-
-      //POST a Comment
-
-
-
-    //   app.post("/comment",async(req,res)=>
-    //   {
-    //     const newUser = req.body;
-    //     console.log('Send to DB',req.body);
-    //     const result = await  commentDataCollection.insertOne(newUser);
-    //     console.log(`A comment was inserted with the _id: ${result.insertedId}`);
-    //     res.json(result)
-    //   })
-     
+   
 
 
       // GET A COMMENT
@@ -222,6 +257,9 @@ app.get('/', (req, res) => {
   }
   run().catch(console.dir);
 
+  // server.listen(port, () => {
+  //   console.log(`My Web is  listening at >>>> http://localhost:${port}`)
+  // })
   app.listen(port, () => {
     console.log(`My Web is  listening at >>>> http://localhost:${port}`)
   })
